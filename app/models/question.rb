6c887c9038d0b09fcefef294eabaa4f762c435ce
Alicia Eyb <aliciaeyb@gmail.com>
2018-05-31 18:27:36 +0200

    first try linking questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :quiz
   belongs_to :parent_answer, class_name: 'Answer', optional: true
-  has_many :answers
+  has_many :answers, dependent: :destroy
 end
 
 
