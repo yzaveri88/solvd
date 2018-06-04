a6e32f625e6f6ef6b6c2b245577a240f20d57b31
AmandaS <mandyschmitz2107@gmail.com>
2018-06-04 18:27:35 +0200

    profile view done, links not working - simple form design not finished

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,36 +2,40 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
   before_action :authenticate_user!, :except => [:home]
 
-
-
   def home
     @hide_footer = true
   end
 
-  def profile
+  def show #profile
     #access to profile page
     @user = current_user
+
   # end
-
-  # def edit
-  #   @user = current_user
-  # end
-
-  # def update
-  @user.update(user_params)
-  redirect_to page_path
-  # end
-
-  # def destroy
-  #   @user = current_user
-  @user.destroy
-  redirect_to  category_index_path
-end
+  end
 
 
-def continue
+  def continue
     # navigation bar - go back to quizzes
     @user = current_user
+  end
+
+  def edit
+    # inside profile view > Overview page to Edit e-mail, Change PW, Deactivate account
+    @user = current_user
+
+    # def edit
+    #   @user = current_user
+    # end
+
+    # def update
+    # @user.update(user_params)
+    #redirect_to page_path
+    # end
+
+    # def destroy
+    #   @user = current_user
+    # @user.destroy
+    #redirect_to  category_index_path
   end
 
   private
