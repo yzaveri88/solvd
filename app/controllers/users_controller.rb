552c4c62f7ee312164f1d97408dfc9e9cbfd81cd
AmandaS <mandyschmitz2107@gmail.com>
2018-06-07 14:35:39 +0200

    Users - show page (favorites) basic layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,11 @@ class UsersController < ApplicationController
   def edit
     @user = current_user
   end
+
+  def show
+    # go back to unfinished quiz (quiz - user_answer - last_question)
+    @user = current_user
+    @sub_categories = SubCategory.all
+    # @user_answer = UserAnswer.find(params[:id])
+  end
 end
